b9ffd6bbe0d8400b995dc8ad6e8c726327d59ffc
Sai Katterishetty <rao.saikrishna@gmail.com>
2016-11-27 22:08:00 -0500

    More Options, for Searching Currencies

--- src/io.rs
+++ src/io.rs
@@ -1,5 +1,5 @@
 use std::io::{self, BufRead, Error, ErrorKind};
-use std::num::ParseFloatError;
+use std::num::{ParseFloatError, ParseIntError};
 use std::collections::HashMap;
 use currency_data_proxy::Currency;
 
@@ -10,9 +10,23 @@ pub struct UserInput {
     pub amount: f64
 }
 
+pub fn print_welcome_banner() {
+    println!(r#"
+ _____                                        _____                           _
+/ ____|                                      / ____|                         | |
+| |    _   _ _ __ _ __ ___ _ __   ___ _   _  | |     ___  _ ____   _____ _ __| |_ ___  _ __
+| |   | | | | '__| '__/ _ | '_ \ / __| | | | | |    / _ \| '_ \ \ / / _ | '__| __/ _ \| '__|
+| |___| |_| | |  | | |  __| | | | (__| |_| | | |___| (_) | | | \ V |  __| |  | || (_) | |
+\_____\__,_|_|  |_|  \___|_| |_|\___|\__, |  \_____\___/|_| |_|\_/ \___|_|   \__\___/|_|
+                                     __/ |
+                                    |___/
+    "#);
+  println!("\n\n");
+}
+
 
 pub fn accept_user_input(available_currencies: &HashMap<String, Currency>) -> Result<UserInput, Error> {
-    println!("Please Enter 3 letter Currency Code you would like to Convert from? ");
+    println!("Please Enter 3 letter Currency Code you would like to Convert from");
 
     let from_currency = (read_currency(&available_currencies))?;
 
@@ -93,5 +107,97 @@ pub fn print_formatted_results(user_input: &UserInput, amt: f64, available_curre
     }
 
     println!("\nPlease serving you!!");
+}
 
+
+pub fn search_country_name<'a>(search_term: &String, country_map: &'a HashMap<String, Currency>) -> Vec<&'a Currency> {
+
+    let mut results: Vec<&Currency> = Vec::new();
+    let search_term_upper: String = search_term.to_uppercase();
+
+    for currency in country_map.values(){
+        if let Some(_) = currency.currency_name.to_uppercase()
+                                    .find(&search_term_upper) {
+            results.push(currency);
+        }
+    }
+    return results;
+}
+
+
+pub fn print_search_results(currencies: &Vec<&Currency>){
+    println!("Ok, We matched a few currencies, here they are");
+
+    for (idx, currency) in currencies.iter().enumerate() {
+        println!("{}. {}", (idx + 1), currency.currency_name);
+    }
+
+    println!("Please Select an Option");
+}
+
+
+
+
+pub fn print_input_options(){
+    println!("Ok, We need a 3 digit currency code e:g USD, GBP, EUR etc \n\n" );
+
+    println!("1. I know the 3 digit currency code");
+    println!("2. I dont know the currency code, I would like to Search by Country");
+}
+
+
+pub fn read_input_option() -> Result<i8, io::Error> {
+
+    println!("Please enter an Option: ");
+
+    let mut user_input: String = String::new();
+    let stdin = io::stdin();
+    let mut stdin_handle = stdin.lock();
+
+    user_input = stdin_handle.read_line(&mut user_input)
+        .map(|_| user_input.trim().to_string()).unwrap();
+
+    if let Ok(x) =  user_input.parse::<i8>() {
+        if x < 1 || x > 2 {
+            return Err(Error::new(ErrorKind::InvalidInput, "Invalid selection"));
+        }
+        else{
+            return Ok(x);
+        }
+    }else{
+        return Err(Error::new(ErrorKind::InvalidInput, "Invalid number entered"));
+    }
+}
+
+pub fn read_search_term() -> Result<String, Error> {
+    let mut user_input: String = String::new();
+    let stdin = io::stdin();
+    let mut stdin_handle = stdin.lock();
+
+    stdin_handle.read_line(&mut user_input)
+        .map(|_| user_input.trim().to_string()).and_then(
+                                                        |input|  if input.len() < 3 {
+                                                            Err(Error::new(ErrorKind::InvalidInput, "Need Atleast 3 characters"))
+                                                        } else
+                                                         { Ok(input)}
+                                                     )
+}
+
+
+// ToDo : Pick up Here
+pub fn accept_search_input(available_currencies: &HashMap<String, Currency>){
+    println!("Please Enter first three letters of the currency");
+
+    let mut from_inp = read_search_term();
+    while let Err(x) = from_inp {
+        println!("Need Atleast 3 characters, Try again");
+        from_inp = read_search_term();
+    }
+
+    let search_results: Vec<&Currency> = search_country_name(&from_inp.unwrap(), available_currencies);
+
+    if search_results.len() == 0 {
+        println!("No Search Results, Try again");
+        from_inp = read_search_term();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,51 @@ pub mod io;
 pub mod currency_data_proxy;
 
 
-use io::{accept_user_input, print_formatted_results, UserInput};
-use currency_data_proxy::{load_all_currencies, get_conversion_rate};
+//use io::{accept_user_input, print_formatted_results, search_country_name, print_search_results, UserInput};
+use io::*;
+use currency_data_proxy::*;
 use std::error::Error;
 
 
 fn main() {
+
+    print_welcome_banner();
     let currency_map = match load_all_currencies() {
         Ok(curr_map) => curr_map,
         Err(error) => panic!("Failed to initialize application Error: {}", error.description())
     };
 
-    let mut user_input_result = accept_user_input(&currency_map);
-
-    while let Err(x) = user_input_result {
-        println!("{}\n\nPlease Try again!  \n\n", x.description());
-        user_input_result = accept_user_input(&currency_map);
+    print_input_options();
+    let mut user_input = read_input_option();
+    while let Err(x) = user_input {
+        println!("Invalid Option, Please try again");
+        user_input = read_input_option();
     }
+
+
+    let user_option = user_input.unwrap();
+
+    if user_option == 1 {
+        let mut user_input_result = accept_user_input(&currency_map);
+
+        while let Err(x) = user_input_result {
+            println!("{}\n\nPlease Try again!  \n\n", x.description());
+            user_input_result = accept_user_input(&currency_map);
+        }
+    } else{
+        accept_search_input(&currency_map);
+    }
+
+    /*
+
+    let input: String = String::from("rup");
+    let vec = search_country_name( &input , &currency_map);
+
+    print_search_results(&vec);
+
+    */
+
+    /*
 
     let user_input: UserInput = user_input_result.unwrap();
 
@@ -31,4 +59,5 @@ fn main() {
 
         print_formatted_results(&user_input, final_val, &currency_map);
     }
+    */
 }
